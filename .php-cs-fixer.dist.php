af178bbec41cda9e7afee0f93dc6505598b3dea4
Ondřej Beneš <benes@wpj.cz>
2023-02-24 08:15:04 +0100

    add php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude([
+        'vendor',
+    ])
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR1' => true,
+        '@PSR2' => true,
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'phpdoc_align' => false,
+        'yoda_style' => [],
+        'increment_style' => false,
+        'no_unneeded_curly_braces' => false,
+        'standardize_increment' => false,
+        'single_line_throw' => false,
+    ])
+    ->setFinder($finder);
