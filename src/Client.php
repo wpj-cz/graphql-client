7ccd727fd0eb7488baf10fc543a82717e7a506fc
Ondrej Benes <benes@wpj.cz>
2025-09-05 09:32:27 +0200

    Update 2.3.0
    - add `Charge` service
    - allow passing custom `httpOptions` in `Client::__construct`
    - refactor: `Client::createServices` now auto-initializes services using reflection

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,7 @@ namespace WpjShop\GraphQL;
 
 use GraphQL\Results;
 use WpjShop\GraphQL\Services\Changes;
+use WpjShop\GraphQL\Services\Charge;
 use WpjShop\GraphQL\Services\Configuration;
 use WpjShop\GraphQL\Services\EditableContent;
 use WpjShop\GraphQL\Services\Order;
@@ -16,6 +17,7 @@ use WpjShop\GraphQL\Services\Reclamation;
 use WpjShop\GraphQL\Services\ReturnDto;
 use WpjShop\GraphQL\Services\Section;
 use WpjShop\GraphQL\Services\Seller;
+use WpjShop\GraphQL\Services\ServiceInterface;
 use WpjShop\GraphQL\Services\Store;
 use WpjShop\GraphQL\Services\Variation;
 
@@ -34,20 +36,23 @@ class Client
     public Configuration $configuration;
     public Variation $variation;
     public Reclamation $reclamation;
+    public Charge $charge;
 
     private \GraphQL\Client $client;
 
-    public function __construct(string $endpoint, string $accessToken)
+    private array $defaultHttpOptions = [
+        'timeout' => 60,
+        'allow_redirects' => ['strict' => true],
+    ];
+
+    public function __construct(string $endpoint, string $accessToken, array $httpOptions = [])
     {
         $this->client = new \GraphQL\Client(
             $endpoint,
             [
                 'X-Access-Token' => $accessToken,
             ],
-            [
-                'timeout' => 60,
-                'allow_redirects' => ['strict' => true],
-            ]
+            array_merge($this->defaultHttpOptions, $httpOptions),
         );
 
         $this->createServices();
@@ -63,36 +68,17 @@ class Client
         return $this->client->runRawQuery($queryString, $resultsAsArray, $variables);
     }
 
-    protected function getServicesClasses(): array
-    {
-        return [
-            Order::class,
-            Product::class,
-            Producer::class,
-            Parameter::class,
-            Section::class,
-            Seller::class,
-            Store::class,
-            EditableContent::class,
-            Changes::class,
-            'return' => ReturnDto::class,
-            Configuration::class,
-            Variation::class,
-            Reclamation::class,
-        ];
-    }
-
     private function createServices(): void
     {
-        foreach ($this->getServicesClasses() as $key => $class) {
-            $service = new $class($this->client);
-            if (is_numeric($key)) {
-                $propertyName = lcfirst((new \ReflectionClass($service))->getShortName());
-            } else {
-                $propertyName = $key;
+        $refClass = new \ReflectionClass($this);
+
+        foreach ($refClass->getProperties(\ReflectionProperty::IS_PUBLIC) as $property) {
+            $class = $property->getType()->getName();
+            if (!is_a($class, ServiceInterface::class, true)) {
+                continue;
             }
 
-            $this->{$propertyName} = $service;
+            $this->{$property->getName()} = new $class($this->client);;
         }
     }
 }
